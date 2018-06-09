7aa9e11a757e4b044b3931b1582aa4730d25bfd7
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2018-06-09 20:15:18 +0900

    ActiveStorageを導入

--- app/models/impression.rb
+++ app/models/impression.rb
@@ -2,4 +2,6 @@ class Impression < ApplicationRecord
   belongs_to :user
   has_many :selecting_emotions, dependent: :destroy
   has_many :emotions, through: :selecting_emotions
+
+  has_one_attached :capture
 end
