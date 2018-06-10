1224dc9e641b4db273a54221a8e027879cb27e89
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2018-06-11 00:15:20 +0900

    アップロードされる画像の調整

--- app/frontend/javascripts/components/impressions/CaptureArea.vue
+++ app/frontend/javascripts/components/impressions/CaptureArea.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="capture">
-    <h3>タイトル</h3>
-    <p>{{ comicTitle }}</p>
-    <h3>感情</h3>
-    <span v-for="emotion in emotions">
-      <span class="message">{{ emotion.name }}</span>
-    </span>
-    <h3>ベストシーン</h3>
-    <p>{{ bestScene }}</p>
-    <h3>感想</h3>
-    <p style="white-space: pre-wrap;">{{ report }}</p>
+  <div class="capture-container">
+    <img src="../../../images/logo.png" height="30" width="128" />
+    <div class="capture-contents">
+      <div class="capture-field">
+        <h3 class="capture-comic-title capture-input">{{ comicTitle }}</h3>
+        <p class="capture-message">を読みました！</p>
+      </div>
+
+      <div class="capture-field_emotion">
+        <div v-for="emotion in emotions" class="capture-emotion-item">
+          <div class="siimple-checkbox">
+            <input :id="`emotion${emotion.id}`" type="checkbox" />
+            <label :for="`emotion${emotion.id}`" style="background: #fff;"></label>
+          </div>
+          <label class="siimple-label">{{ emotion.name }}</label>
+        </div>
+      </div>
+
+      <div class="capture-field">
+        <h3 class="capture-sub-title">ベストシーン</h3>
+        <p class="capture-message capture-input">{{ bestScene }}</p>
+      </div>
+
+      <div class="capture-field">
+        <h3 class="capture-sub-title">感想</h3>
+        <p class="capture-message capture-input">{{ report }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
