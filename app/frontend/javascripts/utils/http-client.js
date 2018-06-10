16872a20254c3a2b15bb47edc8070a76fec19289
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2018-06-10 20:07:25 +0900

    uploader修正

--- app/frontend/javascripts/utils/http-client.js
+++ app/frontend/javascripts/utils/http-client.js
@@ -3,6 +3,7 @@ import { csrfToken } from 'rails-ujs';
 
 console.log(csrfToken())
 export const axios = axiosBase.create({
+  // baseURL: 'http://localhost:3000/api',
   baseURL: 'https://kuriyama-app.herokuapp.com/api',
   responseType: 'json',
   headers: {
