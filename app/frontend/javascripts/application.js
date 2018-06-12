8d46a8fb2a116bb783eb22a12d478d8a2d9fa24e
Yoshihiro Kimura <kimura@diveintocode.jp>
2018-06-12 09:48:52 +0900

    ドロップダウンメニュー設置

--- app/frontend/javascripts/application.js
+++ app/frontend/javascripts/application.js
@@ -1,5 +1,13 @@
-import 'stylesheets/application';
+import Vue from 'vue/dist/vue.esm';
+import ElementUI from 'element-ui';
 import Rails from 'rails-ujs';
+import 'element-ui/lib/theme-chalk/index.css';
+import 'stylesheets/application';
 require.context('../images', true, /\.(png|jpg|jpeg|svg)$/);
 
 Rails.start();
+Vue.use(ElementUI);
+
+new Vue({
+  el: '#js-header',
+});
