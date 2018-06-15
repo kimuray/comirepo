398f3d256b37bd0dc934267a84d572771ed1e9d6
Yoshihiro Kimura <kimura@diveintocode.jp>
2018-06-15 09:11:58 +0900

    不要なルーティングを削除

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -1,6 +1,5 @@
 class ImpressionsController < ApplicationController
   before_action :authenticate_user!, except: :show
-  before_action :set_twitter_api, only: :create
 
   def show
     @impression = Impression.find(params[:id])
@@ -10,21 +9,7 @@ class ImpressionsController < ApplicationController
     @impression = Impression.new
   end
 
-  def create
-    @impression = current_user.impressions.build(impression_params)
-    if @impression.save
-      @twitter_api.tweet(@impression.report)
-      redirect_to root_url, notice: '感想文を提出しました'
-    else
-      render :new
-    end
-  end
-
   private
-
-  def set_twitter_api
-    @twitter_api = TwitterApi.new(current_user.access_token, current_user.access_token_secret)
-  end
 
   def impression_params
     params.fetch(:impression, {}).permit(:comic_title, :best_scene, :report, { emotion_ids: [] })
--- app/frontend/javascripts/utils/http-client.js
+++ app/frontend/javascripts/utils/http-client.js
@@ -2,8 +2,8 @@ import axiosBase from 'axios';
 import { csrfToken } from 'rails-ujs';
 
 export const axios = axiosBase.create({
-  // baseURL: 'http://localhost:3000/api',
-  baseURL: 'https://kuriyama-app.herokuapp.com/api',
+  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'https://kuriyama-app.herokuapp.com/api',
   responseType: 'json',
   headers: {
     'X-CSRF-Token': csrfToken(),
