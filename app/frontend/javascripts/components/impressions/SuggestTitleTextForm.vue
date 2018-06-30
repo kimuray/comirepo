2050688d64da75b1520a200a0ddd8651e061bdd5
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2018-06-30 16:22:49 +0900

    エンター押した時の動作などを調整

--- app/frontend/javascripts/components/impressions/SuggestTitleTextForm.vue
+++ app/frontend/javascripts/components/impressions/SuggestTitleTextForm.vue
@@ -7,9 +7,14 @@
       name="impression[comic_title]"
       class="siimple-input post-form-fluid"
       @input="filterComicTitle"
+      @keyup.enter="enterComicTitle"
+      @blur="deleteSuggestList"
       required />
-    <div class="post-form-comic-title-form-suggest" v-show="isDisplaySuggest">
-      <p v-for="title in suggestTitleList" class="post-form-suggest-item">
+    <div class="post-form-comic-title-form-suggest" v-if="isDisplaySuggest">
+      <p
+        v-for="title in suggestTitleList"
+        @click="selectSuggestTitle"
+        class="post-form-suggest-item">
         {{ title }}
       </p>
     </div>
@@ -34,15 +39,32 @@ export default {
   methods: {
     filterComicTitle(e) {
       if (e.target.value === '') {
-        this.suggestTitleList = []
-        this.isDisplaySuggest = this.suggestTitleList.length > 0
-        return
+        this.suggestTitleList = [];
+        this.deleteSuggestList();
+        return;
       }
       ComicsApi.filterTitle(e.target.value).then(res => {
         this.suggestTitleList = res.data;
         this.isDisplaySuggest = this.suggestTitleList.length > 0
       });
     },
+    enterComicTitle(e) {
+      e.preventDefault();
+      if (!!this.suggestTitleList[0]) {
+        this.comicTitle = this.suggestTitleList[0];
+      }
+      this.deleteSuggestList();
+    },
+    selectSuggestTitle(e) {
+      this.comicTitle = e.target.innerText;
+      this.deleteSuggestList();
+    },
+    deleteSuggestList() {
+      // FIXME: blurの動作の為に一瞬の待ち時間が必要
+      setTimeout(() => {
+        this.isDisplaySuggest = false;
+      }, 50);
+    }
   }
 }
 </script>
