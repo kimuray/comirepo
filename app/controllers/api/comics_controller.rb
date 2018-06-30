c9d0ed40a7072f9394c0897df69fe79d42dba7b0
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2018-07-01 00:33:42 +0900

    漫画タイトル検索の発行SQLを修正

--- app/controllers/api/comics_controller.rb
+++ app/controllers/api/comics_controller.rb
@@ -1,6 +1,6 @@
 class Api::ComicsController < ApplicationController
   def index
-    @comics = Comic.like_title_kana(params[:word]).limit(10)
+    @comics = Comic.search_title(params[:word]).limit(10)
     render json: @comics.map { |comic| comic.title }
   end
 end
--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,5 +1,13 @@
 class Comic < ApplicationRecord
+  scope :like_title, ->(word) {
+    where('title LIKE :word', word: "#{word}%")
+  }
   scope :like_title_kana, ->(word) {
-    where('title_kana LIKE :word', word: "%#{word}%")
+    where('title_kana LIKE :word', word: "#{word}%")
+  }
+  scope :search_title, ->(word) {
+    Comic
+      .like_title(word)
+      .or(Comic.like_title_kana(word))
   }
 end
