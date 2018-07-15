ea16743d2be89079ba09a6291f056226ee96e546
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2018-07-16 03:29:24 +0900

    ogp画像等の調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,25 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'comirepo',
-      title: '漫画の感想投稿サービス',
+      site: 'まんがのおばけ',
+      title: 'まんがのおばけ',
       reverse: true,
       charset: 'utf-8',
-      description: '漫画の感想をかけるサイトです',
+      description: '漫画の感想をフォロワーに向けて発信するサービスです。漫画は他人と感想を共有してこそ面白い！',
       keywords: '漫画,コミック,感想',
       canonical: request.original_url,
       separator: '|',
-      # icon: [
-      #   { href: image_url('favicon.ico') },
+      icon: [
+         { href: image_url('/favicon.ico') },
       #   { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
-      # ],
+      ],
       og: {
-        site_name: 'comirepo',
-        title: '漫画の感想投稿サービス',
-        description: '漫画の感想が書けるサイトです',
+        site_name: 'まんがのおばけ',
+        title: 'まんがのおばけ',
+        description: '漫画の感想をフォロワーに向けて発信するサービスです。漫画は他人と感想を共有してこそ面白い！',
         type: 'website',
         url: request.original_url,
-        # image: image_url('ogp.png'),
+        image: image_url('/ogp.png'),
         locale: 'ja_JP',
       },
       twitter: {
